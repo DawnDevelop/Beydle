9881fe180aa1a2dc0856167c56746df5690aee2a
DawnDevelop <denizjoecks@yahoo.de>
2026-09-21 13:19:33 +0200

    fixes

--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Beydle;
 using Beydle.Services;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
